5144683223ac6e600a3cafb9241d413dde392997
mieszko4 <mieszko4@gmail.com>
2018-08-24 22:18:48 +0200

    Add dummy method

--- android/src/main/java/ch/milosz/reactnative/RNZoomUsModule.java
+++ android/src/main/java/ch/milosz/reactnative/RNZoomUsModule.java
@@ -1,6 +1,9 @@
 
 package ch.milosz.reactnative;
 
+import java.lang.Math;
+
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
@@ -13,6 +16,21 @@ public class RNZoomUsModule extends ReactContextBaseJavaModule {
   public RNZoomUsModule(ReactApplicationContext reactContext) {
     super(reactContext);
     this.reactContext = reactContext;
+  }
+
+  /**
+   * PUBLIC REACT API
+   *
+   *  getRandomNumber()   Returns a random number
+   */
+  @ReactMethod
+  public void getRandomNumber(final Promise promise) {
+    try {
+      double n = Math.random();
+      promise.resolve(n);
+    } catch (Exception ex) {
+      promise.reject("ERR_UNEXPECTED_EXCEPTION", ex);
+    }
   }
 
   @Override
