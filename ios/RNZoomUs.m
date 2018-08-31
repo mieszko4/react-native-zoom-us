6fc86f36a50c9c75cf1c4144df7d488b5da784bf
mieszko4 <mieszko4@gmail.com>
2018-08-31 16:12:40 +0200

    Initialization

--- ios/RNZoomUs.m
+++ ios/RNZoomUs.m
@@ -2,12 +2,51 @@
 #import "RNZoomUs.h"
 
 @implementation RNZoomUs
+{
+  RCTPromiseResolveBlock initializePromiseResolve;
+  RCTPromiseRejectBlock initializePromiseReject;
+  BOOL isInitialized;
+}
+
+- (instancetype)init {
+  if (self = [super init]) {
+    isInitialized = NO;
+    initializePromiseResolve = nil;
+    initializePromiseReject = nil;
+  }
+  return self;
+}
+
++ requiresMainQueueSetup
+{
+  return NO;
+}
 
 - (dispatch_queue_t)methodQueue
 {
-    return dispatch_get_main_queue();
+  return dispatch_get_main_queue();
 }
-RCT_EXPORT_MODULE()
 
+RCT_EXPORT_MODULE(
+  NSString *appKey,
+  NSString *appSecret,
+  NSString *webDomain,
+  RCTPromiseResolveBlock resolve,
+  RCTPromiseRejectBlock reject
+)
+{
+  if (isInitialized) {
+    resolve(@"Already initialize Zoom SDK successfully.");
+    return;
+  }
+
+  isInitialized = true;
+
+  // TODO: try catch with reject("ERR_UNEXPECTED_EXCEPTION", ex);
+  // TODO: zoomsdk init code
+  initializePromiseResolve = resolve;
+  initializePromiseReject = reject;
+
+  resolve(@"DOING IT!");
+}
 @end
-  
