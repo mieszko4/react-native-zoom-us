4f571990200557ee761cec0b02d7b4d27ff83838
Franklin <franklin@firefly.health>
2020-03-25 16:01:52 -0400

    Updating xcodeproj and header files

--- ios/RNZoomUs.h
+++ ios/RNZoomUs.h
@@ -1,10 +1,5 @@
 
-#if __has_include("RCTBridgeModule.h")
 #import "RCTBridgeModule.h"
-#else
-#import <React/RCTBridgeModule.h>
-#endif
-
 #import <MobileRTC/MobileRTC.h>
 
 @interface RNZoomUs : NSObject <RCTBridgeModule, MobileRTCAuthDelegate, MobileRTCMeetingServiceDelegate>
