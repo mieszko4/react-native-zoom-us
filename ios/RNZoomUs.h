837a60def6e36b2b333d4b475e81379554ffbd47
Ofri Ivzori <ofri@7chairs.org>
2020-11-05 18:12:29 +0200

    added ios event callbacks as well

--- ios/RNZoomUs.h
+++ ios/RNZoomUs.h
@@ -7,7 +7,15 @@
 
 #import <MobileRTC/MobileRTC.h>
 
-@interface RNZoomUs : NSObject <RCTBridgeModule, MobileRTCAuthDelegate, MobileRTCMeetingServiceDelegate>
+#if __has_include(<React/RCTEventEmitter.h>)
+#import <React/RCTEventEmitter.h>
+#elif __has_include("React/RCTEventEmitter.h")
+#import "React/RCTEventEmitter.h"
+#else
+#import "RCTEventEmitter.h"
+#endif
+
+@interface RNZoomUs : RCTEventEmitter <RCTBridgeModule, MobileRTCAuthDelegate, MobileRTCMeetingServiceDelegate, MobileRTCVideoServiceDelegate, MobileRTCUserServiceDelegate >
 
 @end
 
