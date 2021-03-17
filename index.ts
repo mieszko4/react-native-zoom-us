633a784cd0f818a0bc59c7da803690b6ba25fa87
Paulo Mogollón <pmogollon@propiedata.com>
2021-03-17 18:19:08 -0500

    Export missing leaveMeeting and connectAudio in index.ts

--- index.ts
+++ index.ts
@@ -82,6 +82,14 @@ async function startMeeting(params: RNZoomUsStartMeetingParams) {
   return RNZoomUs.startMeeting({ userType, ...params, meetingNumber })
 }
 
+async function leaveMeeting() {
+  return RNZoomUs.leaveMeeting()
+}
+
+async function connectAudio() {
+  return RNZoomUs.connectAudio()
+}
+
 export default {
   ...RNZoomUs,
   initialize,
