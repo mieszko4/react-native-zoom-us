5709e7f0ae59826df947c6f37d6390cd25f7f5c6
AnushaGurajapu <anusha@egnify.com>
2021-06-18 12:02:48 +0530

    added MeetingViewsOptions for start meeting

--- android/src/main/java/ch/milosz/reactnative/RNZoomUsModule.java
+++ android/src/main/java/ch/milosz/reactnative/RNZoomUsModule.java
@@ -144,6 +144,17 @@ public class RNZoomUsModule extends ReactContextBaseJavaModule implements ZoomSD
       }
 
       StartMeetingOptions opts = new StartMeetingOptions();
+      MeetingViewsOptions view = new MeetingViewsOptions();
+
+      if(paramMap.hasKey("noInvite")) opts.no_invite = paramMap.getBoolean("noInvite");
+
+      if(paramMap.hasKey("noButtonLeave") && paramMap.getBoolean("noButtonLeave")) opts.meeting_views_options = opts.meeting_views_options + view.NO_BUTTON_LEAVE;
+      if(paramMap.hasKey("noButtonMore") && paramMap.getBoolean("noButtonMore")) opts.meeting_views_options = opts.meeting_views_options + view.NO_BUTTON_MORE;
+      if(paramMap.hasKey("noButtonParticipants") && paramMap.getBoolean("noButtonParticipants")) opts.meeting_views_options = opts.meeting_views_options + view.NO_BUTTON_PARTICIPANTS;
+      if(paramMap.hasKey("noButtonShare") && paramMap.getBoolean("noButtonShare")) opts.meeting_views_options = opts.meeting_views_options + view.NO_BUTTON_SHARE;
+      if(paramMap.hasKey("noTextMeetingId") && paramMap.getBoolean("noTextMeetingId")) opts.meeting_views_options = opts.meeting_views_options + view.NO_TEXT_MEETING_ID;
+      if(paramMap.hasKey("noTextPassword") && paramMap.getBoolean("noTextPassword")) opts.meeting_views_options = opts.meeting_views_options + view.NO_TEXT_PASSWORD;
+
       StartMeetingParamsWithoutLogin params = new StartMeetingParamsWithoutLogin();
       params.displayName = paramMap.getString("userName");
       params.meetingNo = paramMap.getString("meetingNumber");
