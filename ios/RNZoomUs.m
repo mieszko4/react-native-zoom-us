50fabb2de70db295561e22b3dfc0e43e1f24f13d
mieszko4 <mieszko4@gmail.com>
2021-10-04 10:55:19 +0200

    Comment out ios

--- ios/RNZoomUs.m
+++ ios/RNZoomUs.m
@@ -424,12 +424,15 @@ RCT_EXPORT_METHOD(lowerMyHand: (RCTPromiseResolveBlock)resolve rejecter:(RCTProm
   }
 }
 
+/*
+NOTE: this is only required for android
 RCT_EXPORT_METHOD(addListener : (NSString *)eventName) {
   // Keep: Required for RN built in Event Emitter Calls.
 }
 RCT_EXPORT_METHOD(removeListeners : (NSInteger)count) {
   // Keep: Required for RN built in Event Emitter Calls.
 }
+*/
 
 - (void)onMobileRTCAuthReturn:(MobileRTCAuthError)returnValue {
   NSLog(@"nZoomSDKInitializeResult, errorCode=%d", returnValue);
