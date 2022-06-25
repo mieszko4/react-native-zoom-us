494c985d5f799d6d440ca9495407634792aea30b
mieszko4 <mieszko4@gmail.com>
2022-06-25 12:09:29 +0200

    feat: convert VideoAspectModeEnum to union type

--- native.ts
+++ native.ts
@@ -7,13 +7,16 @@ import {
   ViewStyle,
 } from 'react-native'
 
-export enum VideoAspectModeEnum {
-  VIDEO_ASPECT_ORIGINAL = 0,
-  VIDEO_ASPECT_FULL_FILLED = 1,
-  VIDEO_ASPECT_LETTER_BOX = 2,
-  VIDEO_ASPECT_PAN_AND_SCAN = 3,
-  VIDEO_ASPECT_TRUSTEE = 4,
-}
+export const VideoAspectModeEnum = {
+  VIDEO_ASPECT_ORIGINAL: 0,
+  VIDEO_ASPECT_FULL_FILLED: 1,
+  VIDEO_ASPECT_LETTER_BOX: 2,
+  VIDEO_ASPECT_PAN_AND_SCAN: 3,
+  VIDEO_ASPECT_TRUSTEE: 4,
+} as const
+
+type Values<T> = T[keyof T];
+export type VideoAspectMode = Values<typeof VideoAspectModeEnum>;
 
 export interface NativeLayoutUnit {
   kind: "active" | "preview" | "share" | "attendee" | "active-share"
@@ -26,7 +29,7 @@ export interface NativeLayoutUnit {
   showAudioOff?: boolean
   userIndex?: number
   background?: string
-  aspectMode?: VideoAspectModeEnum // buggy typing
+  aspectMode?: VideoAspectMode
 }
 
 export interface NativeVideoProps {
