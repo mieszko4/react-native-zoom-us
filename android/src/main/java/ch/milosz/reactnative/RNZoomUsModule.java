235d009a7a074e987c757aca65211ea9c5bd3b03
mieszko4 <mieszko4@gmail.com>
2022-06-27 22:29:30 +0200

    Clean promise logic and shouldAutoConnectAudio in joinMeeting

--- android/src/main/java/ch/milosz/reactnative/RNZoomUsModule.java
+++ android/src/main/java/ch/milosz/reactnative/RNZoomUsModule.java
@@ -77,7 +77,7 @@ public class RNZoomUsModule extends ReactContextBaseJavaModule implements ZoomSD
   private final static int SCREEN_SHARE_REQUEST_CODE = 99;
   private final ReactApplicationContext reactContext;
 
-  private Boolean shouldAutoConnectAudio = false;
+  private Boolean shouldAutoConnectAudio;
   private Promise initializePromise;
   private Promise meetingPromise;
 
@@ -279,16 +279,13 @@ public class RNZoomUsModule extends ReactContextBaseJavaModule implements ZoomSD
     final ReadableMap paramMap,
     Promise promise
   ) {
-    meetingPromise = promise;
-    shouldAutoConnectAudio = paramMap.getBoolean("autoConnectAudio");
-
     UiThreadUtil.runOnUiThread(new Runnable() {
       @Override
       public void run() {
         try {
           ZoomSDK zoomSDK = ZoomSDK.getInstance();
           if(!zoomSDK.isInitialized()) {
-            meetingPromise.reject("ERR_ZOOM_JOIN", "ZoomSDK has not been initialized successfully");
+            promise.reject("ERR_ZOOM_JOIN", "ZoomSDK has not been initialized successfully");
             return;
           }
 
@@ -342,14 +339,24 @@ public class RNZoomUsModule extends ReactContextBaseJavaModule implements ZoomSD
           if(paramMap.hasKey("password")) params.password = paramMap.getString("password");
           if(paramMap.hasKey("webinarToken")) params.webinarToken = paramMap.getString("webinarToken");
 
+          // Save promise and shouldAutoConnectAudio so that it can be resolved in onMeetingStatusChanged
+          // after zoomSDK.joinMeetingWithParams is called
+          meetingPromise = promise;
+          shouldAutoConnectAudio = paramMap.getBoolean("autoConnectAudio");
           int joinMeetingResult = meetingService.joinMeetingWithParams(reactContext.getCurrentActivity(), params, opts);
           Log.i(TAG, "joinMeeting, joinMeetingResult=" + joinMeetingResult);
 
           if (joinMeetingResult != MeetingError.MEETING_ERROR_SUCCESS) {
+            // We are resolving promise: (1) right away and (2) in onMeetingStatusChanged because in case of no success onMeetingStatusChanged will not be triggered
+            // It is not clear from docs (https://marketplace.zoom.us/docs/sdk/native-sdks/android/mastering-zoom-sdk/start-join-meeting/join-meeting)
             meetingPromise.reject("ERR_ZOOM_JOIN", "joinMeeting, errorCode=" + joinMeetingResult);
+            meetingPromise = null;
+            shouldAutoConnectAudio = null;
           }
         } catch (Exception ex) {
-          meetingPromise.reject("ERR_UNEXPECTED_EXCEPTION", ex);
+          promise.reject("ERR_UNEXPECTED_EXCEPTION", ex);
+          meetingPromise = null;
+          shouldAutoConnectAudio = null;
         }
       }
     });
@@ -787,14 +794,16 @@ public class RNZoomUsModule extends ReactContextBaseJavaModule implements ZoomSD
               "Error: " + errorCode + ", internalErrorCode=" + internalErrorCode
       );
       meetingPromise = null;
+
       shouldAutoConnectAudio = null;
     } else if (meetingStatus == MeetingStatus.MEETING_STATUS_INMEETING) {
       meetingPromise.resolve("Connected to zoom meeting");
       meetingPromise = null;
 
-      if (shouldAutoConnectAudio == true) {
+      if (shouldAutoConnectAudio != null && shouldAutoConnectAudio == true) {
         connectAudioWithVoIP();
       }
+      shouldAutoConnectAudio = null;
     }
   }
 
