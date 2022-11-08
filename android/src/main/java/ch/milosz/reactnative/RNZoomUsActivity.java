d94fa6e7a7cdd28fe229ec2b00327ebab9dc02ae
sridhar <sridhar@learnyst.com>
2022-11-08 15:46:59 +0530

    Update RNZoomUsActivity.java

--- android/src/main/java/ch/milosz/reactnative/RNZoomUsActivity.java
+++ android/src/main/java/ch/milosz/reactnative/RNZoomUsActivity.java
@@ -2,8 +2,18 @@ package ch.milosz.reactnative;
 
 import us.zoom.sdk.ZoomSDK;
 import us.zoom.sdk.MeetingActivity;
+import android.view.WindowManager;
+import android.os.Bundle;
 
 public class RNZoomUsActivity extends MeetingActivity {
+
+  /*onCreate Added by Sridhar*/
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
+  }
+
   @Override
   public void onBackPressed() {
     onClickLeave();
