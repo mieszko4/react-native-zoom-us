ba503cb4455bda4db77272ef9034eaf32ba0f19b
Will Wilkinson <40211746+wilkinson4@users.noreply.github.com>
2024-01-11 19:03:51 -0600

    feat: upgrade Android Zoom SDK to 5.16.2.16555 (#314)
    * Upgrade zoom sdk to 5.16.2.16555
    
    * updateVideoView was causing issues when leaving meetings. Removing it
    resolves them.
    
    * Update CHANGELOG
    
    * Update node dependencies
    
    * Fix issue when non-host leaves meeting and it tries to reconnect.
    
    ---------
    
    Co-authored-by: William Wilkinson <will.wilkinson@carebridgehealth.com>

--- android/src/main/java/ch/milosz/reactnative/RNZoomUsModule.java
+++ android/src/main/java/ch/milosz/reactnative/RNZoomUsModule.java
@@ -25,6 +25,7 @@ import com.facebook.react.uimanager.UIBlock;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 import java.lang.Long;
+import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -49,6 +50,7 @@ import us.zoom.sdk.ZoomSDKInitParams;
 import us.zoom.sdk.FreeMeetingNeedUpgradeType;
 import us.zoom.sdk.ShareSettingType;
 import us.zoom.sdk.IRequestLocalRecordingPrivilegeHandler;
+import us.zoom.sdk.LocalRecordingRequestPrivilegeStatus;
 
 import us.zoom.sdk.SharingStatus;
 import us.zoom.sdk.MeetingStatus;
@@ -180,17 +182,11 @@ public class RNZoomUsModule extends ReactContextBaseJavaModule implements ZoomSD
           zoomSDK.setSdkLocale(reactContext, locale);
 
           ZoomSDKInitParams initParams = new ZoomSDKInitParams();
-          if (params.hasKey("jwtToken")) {
-            initParams.jwtToken = params.getString("jwtToken");
-            initParams.domain = params.getString("domain");
-            // initParams.enableLog = true;
-            // initParams.enableGenerateDump =true;
-            // initParams.logSize = 5;
-          } else {
-            initParams.appKey = params.getString("clientKey");
-            initParams.appSecret = params.getString("clientSecret");
-            initParams.domain = params.getString("domain");
-          }
+          initParams.jwtToken = params.getString("jwtToken");
+          initParams.domain = params.getString("domain");
+          // initParams.enableLog = true;
+          // initParams.enableGenerateDump =true;
+          // initParams.logSize = 5;
 
           // Save promise so that it can be resolved in onZoomSDKInitializeResult
           // after zoomSDK.initialize is called
@@ -363,7 +359,6 @@ public class RNZoomUsModule extends ReactContextBaseJavaModule implements ZoomSD
           if (paramMap.hasKey("password")) params.password = paramMap.getString("password");
           if (paramMap.hasKey("webinarToken")) params.webinarToken = paramMap.getString("webinarToken");
           if (paramMap.hasKey("zoomAccessToken")) params.zoomAccessToken = paramMap.getString("zoomAccessToken");
-          
 
           // Save promise and shouldAutoConnectAudio so that it can be resolved in onMeetingStatusChanged
           // after zoomSDK.joinMeetingWithParams is called
@@ -1031,7 +1026,22 @@ public class RNZoomUsModule extends ReactContextBaseJavaModule implements ZoomSD
     }
   }
 
+  @Override
+  public void onInMeetingUserAvatarPathUpdated(long userId) {}
+  @Override
+  public void onSuspendParticipantsActivities() {}
+  @Override
+  public void onAllowParticipantsStartVideoNotification(boolean allow) {}
+  @Override
+  public void onAllowParticipantsRenameNotification(boolean allow) {}
+  @Override
+  public void onAllowParticipantsUnmuteSelfNotification(boolean allow) {}
+  @Override
+  public void onAllowParticipantsShareWhiteBoardNotification(boolean allow) {}
+  @Override
+  public void onMeetingLockStatus(boolean isLock) {}
   // InMeetingServiceListener required listeners but unused for now
+
   @Override
   public void onFollowHostVideoOrderChanged(boolean bFollow) {}
   @Override
@@ -1117,7 +1127,8 @@ public class RNZoomUsModule extends ReactContextBaseJavaModule implements ZoomSD
   public void onShareMeetingChatStatusChanged(boolean start) {}
   @Override
   public void onLocalRecordingPrivilegeRequested(IRequestLocalRecordingPrivilegeHandler handler) {}
-
+  @Override
+  public void onRequestLocalRecordingPrivilegeChanged(LocalRecordingRequestPrivilegeStatus status) {}
 
   // InMeetingShareListener event listeners
   // DEPRECATED: onShareActiveUser is just kept for now for backwards compatibility of events
@@ -1194,7 +1205,8 @@ public class RNZoomUsModule extends ReactContextBaseJavaModule implements ZoomSD
           }
 
           final MeetingService meetingService = zoomSDK.getMeetingService();
-          if(meetingService.getMeetingStatus() != MeetingStatus.MEETING_STATUS_IDLE ) {
+          List<MeetingStatus> staleMeetingStatuses = new ArrayList<>(Arrays.asList(MeetingStatus.MEETING_STATUS_IDLE, MeetingStatus.MEETING_STATUS_DISCONNECTING));
+          if(!staleMeetingStatuses.contains(meetingService.getMeetingStatus())) {
             Log.i(TAG, "onHostResume, returning to meeting");
             meetingService.returnToMeeting(reactContext.getCurrentActivity());
           }
